0da1577eab5d3d8ba4d59a2daa419cab7b3f9c5c
ShivaThallapelli3 <163966141+ShivaThallapelli3@users.noreply.github.com>
2025-04-05 15:55:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const UPLOADS_DIR = process.env.UPLOADS_DIR || "uploads";
 
 // Middleware
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://shareeasy-frontend.onrender.com" }));
 app.use(express.json());
 
 // Connect to MongoDB
